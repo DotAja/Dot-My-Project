531b1fa4e94e4ea2269c3d9bb0456f9d5198313c
DotAja <142836390+DotAja@users.noreply.github.com>
2024-01-28 03:24:23 +0700

    Update runminer.sh

--- runminer.sh
+++ runminer.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
-echo "tanya owner untuk kata sandi"
+
+echo "Tanya owner untuk kata sandi"
 echo "Masukkan kata sandi:"
 read -s script_password
 
 if [ "$script_password" == "dot" ]; then
     echo "Kata sandi benar"
     clear
+    echo "Masukkan nama worker klean:"
+    read worker_name
+    
     echo "Masukkan wallet klean:"
     read wallet_address
     
     # Sisanya dari skrip Anda di sini
-    wget https://github.com/doktor83/SRBMiner-Multi/releases/download/2.3.5/SRBMiner-Multi-2-3-5-Linux.tar.xz
-    tar -xvf SRBMiner-Multi-2-3-5-Linux.tar.xz
-    cd SRBMiner-Multi-2-3-5
+    wget https://github.com/cpu-pool/cpuminer-opt-cpupower/releases/download/1.4/Cpuminer-opt-cpu-pool-linux64.tar.gz
+    tar zxvf Cpuminer-opt-cpu-pool-linux64.tar.gz
     clear
     echo -e "\033[1;31m ##############################################"
     echo "#####      #####    ########                            "
@@ -22,7 +25,7 @@ if [ "$script_password" == "dot" ]; then
     echo "##   ##  ##     ##     ##        ######     ##   ###### "
     echo "#####      #####       ##        ##  ##   ##     ##  ## "
     echo -e "            \033[1;32m  MINING BERJALAN...\n\n"
-    ./SRBMiner-MULTI -a yespower -o stratum+tcp://na.mining4people.com:3338 --wallet $wallet_address
+    ./cpuminer -a yespower -o stratum+tcp://na.mining4people.com:3338 --wallet $wallet_address.$worker_name
 else
     echo "Kata sandi salah tolol"
 fi
